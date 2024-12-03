a0d8fa4ada2f29adba70fc306fce7decf883b1fd
AlexMickh <sanchespanches33@gmail.com>
2024-12-04 01:26:34 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "glad.h"
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    GLFWwindow *window;
+
+    if (!glfwInit()) 
+    {
+        std::exit(1);
+    }
+
+    window = glfwCreateWindow(640, 480, "Hello, OpenGL!", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Couldn't load OpenGL" << std::endl;
+        glfwTerminate();
+        std::exit(1);
+    }
+
+    glClearColor(0.25f,0.5f, 0.75f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
